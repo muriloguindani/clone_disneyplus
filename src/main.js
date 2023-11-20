a9a606c1791f67589e3c8de85dbb4e0a6ae5c229
Murilo Guindani <murilo.guindani@gmail.com>
2023-11-20 15:12:12 -0300

    criação header

--- src/main.js
+++ src/main.js
@@ -2,7 +2,22 @@ document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
     const tabsContainer = document.querySelectorAll('[data-tab-id]'); 
     const questions = document.querySelectorAll('[data-faq-question]')
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual < alturaHero){
+            ocultaElementosHeader();
+        }
+        else{
+            exibeElementosHeader();
+        }
+    })
     
+    //secão atrações, programação das abas
     for(let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
@@ -19,6 +34,7 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 })
 
+//seção faq, accordion
 function abreOuFechaResposta(elemento){
     const classe = 'faq__questions__item--is-open';
     const elementoPai = elemento.target.parentNode;
@@ -40,4 +56,14 @@ function escondeAbas(){
     for(let i = 0; i < tabsContainer.length; i++){
         tabsContainer[i].classList.remove('shows__list--is-active')
     }
+}
+
+function ocultaElementosHeader(){
+    document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosHeader(){
+    document.querySelector('header');
+    header.classList.remove('header--is-hidden');
 }
